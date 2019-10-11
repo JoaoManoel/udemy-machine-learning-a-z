1f94f635ae190ae2ef1e73ddf6ef314c273a4a01
João Manoel Lins <joaomanoellins@gmail.com>
2019-10-10 23:36:19 -0300

    Step 1 - Enconding Categorial Data / Avoiding Dummy Variable Trap and Splitting the dataset

---
+++ part-3/multiple_linear_regression.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# Multiple Linear Regression
+
+#Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+# Enconding Categorical data
+# Enconding The Idenpendant Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_X = LabelEncoder()
+X[:, 3] = labelencoder_X.fit_transform(X[:, 3])
+
+onehotencoder = OneHotEncoder(categorical_features=[3])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Avoiding the Dummy Varible Trap
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)###
