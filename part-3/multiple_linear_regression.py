7794f0a74e47c88aff25d92d8509140689444afd
João Manoel Lins <joaomanoellins@gmail.com>
2019-10-11 22:19:02 -0300

    Backward Elimination

--- part-3/multiple_linear_regression.py
+++ part-3/multiple_linear_regression.py
@@ -36,9 +36,17 @@ regressor.fit(X_train, y_train)
 # Predicting the Test set results
 y_pred = regressor.predict(X_test)
 
+# Building the optimal model using Backward Elimination
+import statsmodels.regression.linear_model as sm
+X = np.append(arr = np.ones((50, 1)).astype(int), values = X, axis = 1)
+X_opt = X[:, [0, 1, 2, 3, 4, 5]]
 
-# Feature scaling
-"""from sklearn.preprocessing import StandardScaler
-sc_X = StandardScaler()
-X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)###
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+
+X_opt = X[:, [0, 1, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+
+X_opt = X[:, [0, 3, 4, 5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+
+regressor_OLS.summary()
