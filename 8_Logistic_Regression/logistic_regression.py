67f3bde7a2abf08dcb094178b6142c5a69567de3
João Manoel Lins <joaomanoellins@gmail.com>
2019-10-26 23:21:32 -0300

    Logistic Regression Part 1 - Python

---
+++ 8_Logistic_Regression/logistic_regression.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+# Logistic Regression
+
+# Importing the Libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Importing the dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+X = dataset.iloc[:, [2, 3]].values
+y = dataset.iloc[:, -1].values
+
+from sklearn.model_selection import train_test_split
+
+# Splitting the dataset into the Training set and Test set
+X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.25, random_state = 0)
+
+# Feature Scaling 
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.fit_transform(X_test)
