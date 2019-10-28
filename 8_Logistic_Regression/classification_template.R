11c489b75c174739bb0e8c1d8eed3a77a719c917
João Manoel Lins <joaomanoellins@gmail.com>
2019-10-28 19:27:41 -0300

    Classification Template - R

--- 8_Logistic_Regression/classification_template.R
+++ 8_Logistic_Regression/classification_template.R
@@ -16,9 +16,8 @@ training_set[, 1:2] = scale(training_set[, 1:2])
 test_set[, 1:2] = scale(test_set[, 1:2])
 
 # Fitting Logistic Regression to the Training Set
-classifier = glm(formula = Purchased ~ .,
-                 family = binomial,
-                 data = training_set)
+# Create your classifier here
+classifier = 
 
 # Predicting the Test set results
 prob_pred = predict(classifier, type = 'response', newdata = test_set[-3])
@@ -36,7 +35,7 @@ colnames(grid_set) = c('Age', 'EstimatedSalary')
 prob_set = predict(classifier, type = 'response', newdata = grid_set)
 y_grid = ifelse(prob_set > 0.5, 1, 0)
 plot(set[, -3],
-     main = 'Logistic Regression (Training Set)',
+     main = 'Classifier (Training Set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
@@ -45,15 +44,15 @@ points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
 
 # Visualising the Test set results
 set = test_set
-X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01) 
-X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01) 
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01)
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01)
 grid_set = expand.grid(X1, X2)
 colnames(grid_set) = c('Age', 'EstimatedSalary')
-prob_set = predict(classifier, type = 'response', newdata = grid_set)
-y_grid = ifelse(prob_set > 0.5, 1, 0)
-plot(set[, -3],
-     main = 'Logistic Regression (Test Set)',
+y_grid = predict(classifier, newdata = grid_set)
+
+plot(set[, -3], main = 'Classifier (Test set)',
      xlab = 'Age', ylab = 'Estimated Salary',
      xlim = range(X1), ylim = range(X2))
 contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
 points(grid_set, pch = '.', col = ifelse(y_grid == 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4', 'red3'))
