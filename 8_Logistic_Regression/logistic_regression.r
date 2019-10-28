d9459e9ee09bb6d4f4afab69f6d0ad21d085b97e
João Manoel Lins <joaomanoellins@gmail.com>
2019-10-28 19:14:15 -0300

    Logistic Regression Step 5 - R

--- 8_Logistic_Regression/logistic_regression.r
+++ 8_Logistic_Regression/logistic_regression.r
@@ -3,6 +3,7 @@ dataset = read.csv('Social_Network_Ads.csv')
 dataset = dataset[, 3:5]
 
 library(caTools)
+library(ElemStatLearn)
 set.seed(123)
 
 # Splitting the dataset into the Training Set
@@ -25,3 +26,19 @@ y_pred = ifelse(prob_pred > 0.5, 1, 0)
 
 # Making the Confusion Matrix
 cm = table(test_set[, 3], y_pred)
+
+# Visualising the Training set results
+set = training_set
+X1 = seq(min(set[, 1]) - 1, max(set[, 1]) + 1, by = 0.01) 
+X2 = seq(min(set[, 2]) - 1, max(set[, 2]) + 1, by = 0.01) 
+grid_set = expand.grid(X1, X2)
+colnames(grid_set) = c('Age', 'EstimatedSalary')
+prob_set = predict(classifier, type = 'response', newdata = grid_set)
+y_grid = ifelse(prob_set > 0.5, 1, 0)
+plot(set[, -3],
+     main = 'Logistic Regression (Training Set)',
+     xlab = 'Age', ylab = 'Estimated Salary',
+     xlim = range(X1), ylim = range(X2))
+contour(X1, X2, matrix(as.numeric(y_grid), length(X1), length(X2)), add = TRUE)
+points(grid_set, pch = '.', col = ifelse(y_grid = 1, 'springgreen3', 'tomato'))
+points(set, pch = 21, bg = ifelse(set[, 3] == 1, 'green4' : 'red3'))
